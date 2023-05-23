af6d6c5f32490141c50486ffaa6cfc8facccb474
Julie Combeau <combeau.julie@gmail.com>
2023-05-23 09:36:02 +0200

    🎉 Initialize project

---
+++ server/src/db.ts
@@ -0,0 +1,9 @@
+import { DataSource } from 'typeorm';
+
+export default new DataSource({
+  type: 'sqlite',
+  database: './wildersdb.sqlite',
+  synchronize: true,
+  entities: [],
+  logging: ['query', 'error'],
+});
---
+++ server/src/index.ts
@@ -0,0 +1,38 @@
+import "reflect-metadata";
+import express from "express";
+import cors from "cors";
+import { ApolloServer } from "apollo-server";
+import datasource from "./db";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+// import { buildSchema } from "type-graphql";
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000" }));
+
+const start = async (): Promise<void> => {
+  await datasource.initialize();
+
+  // const schema = await buildSchema({
+  //   resolvers: [],
+  // });
+
+  const server = new ApolloServer({
+    // schema,
+    csrfPrevention: true,
+    cache: "bounded",
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+  });
+
+  await server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+
+  app.listen(5001, () => {
+    console.log("listening on port 5001");
+  });
+};
+
+void start();
